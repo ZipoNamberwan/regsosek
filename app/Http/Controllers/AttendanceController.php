ce3453c8b5c6c44c912ef6b1c42bc96df96e436a
Zipo Namberwan <dopokan@gmail.com>
2023-01-25 00:04:45 +0700

    add attendance function

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Attendance;
+use App\Models\StatusAttendance;
 use DateInterval;
 use DatePeriod;
 use DateTime;
@@ -27,14 +28,55 @@ class AttendanceController extends Controller
         $attendanceToday = Attendance::where(['user_id' => Auth::user()->id])->where(['date' => $now->format('Y-m-d')])->first();
         $in = null;
         $out = null;
+        $statatttoday = null;
+        $note = null;
         if ($attendanceToday != null) {
             $in = $attendanceToday->in != null ? (new DateTime($attendanceToday->in))->format('H:i') : null;
             $out = $attendanceToday->out != null ? (new DateTime($attendanceToday->out))->format('H:i') : null;
+            $statatttoday = $attendanceToday->statusattdetail;
+            $note = $attendanceToday->note;
         }
 
-        // dd($attendanceToday);
+        $statatts = StatusAttendance::all();
 
-        return view('attendance/index', ['in' => $in, 'out' => $out]);
+        //generate num no attendance
+        $first = new DateTime('2023-01-05');
+        $last = new DateTime(date('Y-m-d H:i:s') . ' -17 hours');
+        $interval = DateInterval::createFromDateString('1 day');
+        $period = new DatePeriod($first, $interval, $last);
+
+        $numnoatt = 0;
+        $daytotal = 0;
+        foreach ($period as $dt) {
+            $row = array();
+            $attendance = Attendance::where(['date' => $dt->format("Y-m-d")])->where(['user_id' => Auth::user()->id])->first();
+            $row['date'] = $dt->format("Y-m-d");
+            $row['in'] = '-';
+            $row['out'] = '-';
+            $row['status'] = '-';
+
+            if ($attendance != null) {
+                $row['in'] = $attendance->in != null ? (new DateTime($attendance->in))->format('H:i') : '-';
+
+                if ($attendance->out != null) {
+                    $format = (new DateTime($attendance->out))->format('Y-m-d') != $dt->format("Y-m-d") ? 'H:i d M' : 'H:i';
+                    $row['out'] = (new DateTime($attendance->out))->format($format);
+                } else {
+                    $row['out'] = '-';
+                }
+                if ($attendance->statusattdetail != null) {
+                    $row['status'] = $attendance->statusattdetail->id;
+                }
+            }
+            if (($row['out'] == '-' || $row['in'] == '-') && $row['status'] == '-') {
+                $numnoatt++;
+            }
+            $daytotal++;
+        }
+
+        // dd($numnoatt);
+
+        return view('attendance/index', ['in' => $in, 'out' => $out, 'statatts' => $statatts, 'statatttoday' => $statatttoday, 'note' => $note, 'numnoatt' => $numnoatt, 'daytotal' => $daytotal]);
     }
 
     /**
@@ -136,7 +178,54 @@ class AttendanceController extends Controller
             ]);
         }
 
-        return redirect('/attendance');
+        return redirect('/attendance')->with('success-success', 'Absensi telah direkam!');;
+    }
+
+    public function markNoAttendance(Request $request)
+    {
+        if ($request->statatt == null) {
+            return redirect('/attendance')->with('success-delete', 'Ketidakhadiran dan catatan belum diisi!');
+        } else {
+            $dateTime = new DateTime(date('Y-m-d H:i:s') . ' +7 hours');
+            $date = $dateTime->format(('Y-m-d'));
+            if ((int)$dateTime->format('H') < 5) {
+                $date = (new DateTime(date('Y-m-d H:i:s') . ' -12 hours'))->format('Y-m-d');
+            }
+
+            $attendanceToday = Attendance::where(['date' => $date])->where(['user_id' => Auth::user()->id])->first();
+            if ($attendanceToday == null) {
+                Attendance::create([
+                    'user_id' => Auth::user()->id,
+                    'date' => $date,
+                    'status_attendance_id' => $request->statatt,
+                    'note' => $request->note,
+                ]);
+            } else {
+                $attendanceToday->update([
+                    'status_attendance_id' => $request->statatt,
+                    'note' => $request->note,
+                ]);
+            }
+
+            return redirect('/attendance')->with('success-create', 'Ketidakhadiran telah disimpan!');
+        }
+    }
+
+    public function cancelNoAttendance()
+    {
+        $dateTime = new DateTime(date('Y-m-d H:i:s') . ' +7 hours');
+        $date = $dateTime->format(('Y-m-d'));
+        if ((int)$dateTime->format('H') < 5) {
+            $date = (new DateTime(date('Y-m-d H:i:s') . ' -12 hours'))->format('Y-m-d');
+        }
+
+        $attendanceToday = Attendance::where(['date' => $date])->where(['user_id' => Auth::user()->id])->first();
+        $attendanceToday->update([
+            'status_attendance_id' => null,
+            'note' => null,
+        ]);
+
+        return redirect('/attendance')->with('success-create', 'Ketidakhadiran dibatalkan, Absensi dibuka kembali!');
     }
 
     public function attendanceData(Request $request)
@@ -145,34 +234,49 @@ class AttendanceController extends Controller
         $last = new DateTime(date('Y-m-d H:i:s') . ' +7 hours');
         $interval = DateInterval::createFromDateString('1 day');
         $period = new DatePeriod($first, $interval, $last);
-
+        $numdate = iterator_count($period);
+        
         $dataArray = array();
         $i = 0;
         foreach ($period as $dt) {
+            $i++;
             $row = array();
             $attendance = Attendance::where(['date' => $dt->format("Y-m-d")])->where(['user_id' => Auth::user()->id])->first();
             $row['date'] = $dt->format("Y-m-d");
-            $row['in'] = '-';
-            $row['out'] = '-';
-            $row['note'] = '-';
+            $row['in'] = null;
+            $row['out'] = null;
+            $row['note'] = null;
+            $row['status_id'] = null;
+            $row['status_name'] = null;
             if ($attendance != null) {
-                $row['in'] = $attendance->in != null ? (new DateTime($attendance->in))->format('H:i') : '-';
+                $row['in'] = $attendance->in != null ? (new DateTime($attendance->in))->format('H:i') : null;
 
                 if ($attendance->out != null) {
                     $format = (new DateTime($attendance->out))->format('Y-m-d') != $dt->format("Y-m-d") ? 'H:i d M' : 'H:i';
                     $row['out'] = (new DateTime($attendance->out))->format($format);
                 } else {
+                    $row['out'] = null;
+                }
+                if ($attendance->statusattdetail != null) {
+                    $row['status_id'] = $attendance->statusattdetail->id;
+                    $row['status_name'] = $attendance->statusattdetail->name;
+                    $row['in'] = '-';
                     $row['out'] = '-';
+                    $row['note'] = $attendance->note ?? '-';
                 }
             }
+            if ($numdate == $i) {
+                $row['out'] = '-';
+            }
             $dataArray[] = $row;
-            $i++;
         }
 
         if ($request->order != null) {
             if ($request->order[0]['dir'] == 'desc') {
                 $dataArray = array_reverse($dataArray);
             }
+        } else {
+            $dataArray = array_reverse($dataArray);
         }
 
         if ($request->length != -1) {
--- resources/views/attendance/index.blade.php
+++ resources/views/attendance/index.blade.php
@@ -39,7 +39,7 @@
     @if (session('success-delete'))
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
         <span class="alert-icon"><i class="fas fa-check-circle"></i></span>
-        <span class="alert-text"><strong>Sukses! </strong>{{ session('success-delete') }}</span>
+        <span class="alert-text"><strong>Gagal! </strong>{{ session('success-delete') }}</span>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">×</span>
         </button>
@@ -48,6 +48,7 @@
 
     <!-- Table -->
     <div class="row">
+        @if($statatttoday == null)
         <div class="col-xl-3 col-md-6">
             <div class="card card-stats">
                 <!-- Card body -->
@@ -120,7 +121,83 @@
                 </div>
             </div>
         </div>
+        <div class="col-xl-3 col-md-6">
+            <div class="card card-stats">
+                <!-- Card body -->
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="card-title text-uppercase text-muted mb-1 mt-2">Tidak Hadir</h5>
+                            <span class="">Gunakan menu ini jika hari ini tidak masuk</span>
+                        </div>
+                        <div class="icon icon-shape bg-gradient-info text-white rounded-circle shadow">
+                            <i class="ni ni-chart-bar-32"></i>
+                        </div>
+                    </div>
+                    <button class="btn btn-icon btn-primary btn-sm mt-3" type="submit" onclick="markNoAttendance()">
+                        <span class="btn-inner--icon"><i class="ni ni-active-40"></i></span>
+                        <span class="btn-inner--text">Rekam</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+        @else
+        <div class="col-xl-3 col-md-6">
+            <div class="card card-stats">
+                <!-- Card body -->
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="card-title text-uppercase text-muted mb-1 mt-2">Tidak Hadir Hari Ini</h5>
+                            <h3 class="font-weight-bold text-uppercase mt-1">{{$statatttoday->name}}</h3>
+                            @if($note != null)<span>{{$note}}</span>@endif
+                        </div>
+                        <div class="icon icon-shape bg-gradient-info text-white rounded-circle shadow">
+                            <i class="ni ni-chart-bar-32"></i>
+                        </div>
+                    </div>
+                    <form id="cancelnoatt" method="post" action="/attendance/noatt/cancel" class="needs-validation" enctype="multipart/form-data" novalidate>
+                        @csrf
+                        @method('post')
+                        <button class="btn btn-icon btn-danger btn-sm mt-3" type="submit" onclick="cancelNoAttendance()">
+                            <span class="btn-inner--icon"><i class="ni ni-active-40"></i></span>
+                            <span class="btn-inner--text">Batalkan</span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        @endif
     </div>
+    @if($numnoatt > 0)
+    <div class="row">
+        <div class="col-xl-3 col-md-6">
+            <div class="card bg-gradient-danger border-0">
+                <!-- Card body -->
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="card-title text-uppercase text-muted mb-2 text-white">Jumlah Ketidakhadiran</h5>
+                            <h5 class="card-title text-muted mb-3 text-white">Menunjukkan jumlah hari tidak absen datang atau pulang. *Hari ini belum dihitung</h5>
+                            <span class="h1 font-weight-bold mb-0 text-white">{{$numnoatt}}/{{$daytotal}} Hari</span>
+                            <div class="progress progress-xs mt-3 mb-0">
+                                <div class="progress-bar bg-success" role="progressbar" aria-valuenow="{{floor($numnoatt/$daytotal * 100)}}" aria-valuemin="0" aria-valuemax="100" style="width: {{floor($numnoatt/$daytotal * 100)}}%;"></div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <button type="button" class="btn btn-sm btn-neutral mr-0" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-ellipsis-h"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-sm">
+                        <a href="#!" class="text-white">Untuk mengurangi jumlah ketidakhadiran, gunakan menu tandai kehadiran manual pada tabel di bawah</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
     <div class="row">
         <div class="col">
             <div class="card">
@@ -140,6 +217,7 @@
                                 <th>Tanggal</th>
                                 <th>Datang</th>
                                 <th>Pulang</th>
+                                <th>Status Ketidakhadiran</th>
                                 <th>Keterangan</th>
                             </tr>
                         </thead>
@@ -195,16 +273,54 @@
                 "width": "5%",
                 "data": "in",
                 "orderable": false,
+                "render": function(data, type, row) {
+                    if (type === 'display') {
+                        if (data == null) {
+                            return `<button class="btn btn-icon btn-primary btn-sm" type="button" onclick="attendanceManualClick('in', '` + row.date + `')">
+                            <span class="btn-inner--icon"><i class="ni ni-active-40"></i></span>
+                            <span class="btn-inner--text">Rekam Manual</span>
+                        </button>`
+                        }
+                    }
+                    return data;
+                }
             },
             {
                 "responsivePriority": 2,
                 "width": "5%",
                 "data": "out",
                 "orderable": false,
+                "render": function(data, type, row) {
+                    if (type === 'display') {
+                        if (data == null) {
+                            return `<button class="btn btn-icon btn-primary btn-sm" type="button" onclick="attendanceManualClick('out', '` + row.date + `')">
+                            <span class="btn-inner--icon"><i class="ni ni-active-40"></i></span>
+                            <span class="btn-inner--text">Rekam Manual</span>
+                        </button>`
+                        }
+                    }
+                    return data;
+                }
             },
             {
                 "responsivePriority": 2,
-                "width": "5%",
+                "width": "3%",
+                "data": "status_id",
+                "orderable": false,
+                "render": function(data, type, row) {
+                    if (type === 'display') {
+                        if (row.status_id != null) {
+                            return "<div class=\"d-flex align-items-center\"><i class=\"fas fa-exclamation-triangle text-warning mr-1\"></i><h3><span class=\"badge badge-warning\">" + row.status_name + "</span></h3></div>";
+                        } else {
+                            return '-'
+                        }
+                    }
+                    return data
+                }
+            },
+            {
+                "responsivePriority": 2,
+                "width": "3%",
                 "data": "note",
                 "orderable": false,
             }
@@ -220,7 +336,6 @@
 
 <script>
     function attendanceFormClick(type) {
-        console.log(type)
         event.preventDefault();
         document.getElementById('loading-background').style.display = 'block'
         $.ajax({
@@ -251,6 +366,87 @@
                 document.getElementById('loading-background').style.display = 'none'
             },
         });
+    }
+</script>
+
+<script>
+    function markNoAttendance() {
+        event.preventDefault();
+        Swal.fire({
+            title: 'Alasan Ketidakhadiran Hari Ini',
+            html: `<form id="noatt" method="post" action="/attendance/noatt" class="needs-validation" enctype="multipart/form-data" novalidate>
+                        @csrf
+                        @method('post') <select id="statatt" name="statatt" class="form-control" data-toggle="select" name="statatt" required>
+                                        <option value="0" disabled selected> -- Pilih Alasan -- </option>
+                                        @foreach ($statatts as $statatt)
+                                        <option value="{{ $statatt->id }}" {{ old('statatt') == $statatt->id ? 'selected' : '' }}>
+                                            {{ $statatt->name }}
+                                        </option>
+                                        @endforeach
+                                    </select>
+                                     <input type="text" id="note" name="note" class="swal2-input" placeholder="Catatan" required> </form>
+            `,
+            inputAttributes: {
+                autocapitalize: 'off'
+            },
+            showCancelButton: true,
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Simpan',
+            cancelButtonText: 'Tidak',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                document.getElementById('noatt').submit();
+            }
+        })
+    }
+</script>
+
+<script>
+    function cancelNoAttendance() {
+        event.preventDefault();
+        Swal.fire({
+            title: 'Batalkan Ketidakhadiran Hari Ini?',
+            text: 'Membatalkan ketidakhadiran akan membuka absen kembali',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ya',
+            cancelButtonText: 'Tidak',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                document.getElementById('cancelnoatt').submit();
+            }
+        })
+    }
+</script>
+
+<script>
+    function attendanceManualClick(type, date) {
+        let maxdate = new Date(date);
+        maxdate.setDate(maxdate.getDate() + 1);
+
+        event.preventDefault();
+        var unixTimestamp = new Date(date).getTime() / 1000 - (new Date).getTimezoneOffset() * 60;
+        dateStr = moment.unix(unixTimestamp).locale('id').format('LL');
+
+        Swal.fire({
+            title: 'Absensi ' + (type == 'in' ? 'Datang' : 'Pulang') + ' Manual Tanggal ' + dateStr,
+            html: `<div class="row"><div class="col mb-2"><input type="date" value="` + date +
+                `" max="` + maxdate.getFullYear() + `-` +
+                String((maxdate.getMonth() + 1)).padStart(2, '0') + `-` + maxdate.getDate() +
+                `" min="` + date + `" class="form-control"></div> <div class="col"><input type="time" class="form-control"></div></div>`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Simpan',
+            cancelButtonText: 'Tidak',
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+            }
+        })
     }
 </script>
 @endsection
